2d1acade06d49eeb9a74bfc09fcc3fdb2340106d
otorres828 <olivertorres1997@gmail.com>
2024-04-28 10:38:40 -0600

    subida

--- src/components/Products/ProductsElements.jsx
+++ src/components/Products/ProductsElements.jsx
@@ -22,7 +22,7 @@ export const ProductsCard = styled.div`
 `;
 
 export const ProductsImg = styled.img`
-	height: 300px;
+	height: 450px;
 	min-width: 300px;
 	max-width: 100%;
 	box-shadow: 8px 8px #fdc500;
