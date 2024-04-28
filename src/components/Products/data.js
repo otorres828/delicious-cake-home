c09c4b3d59eb45b4810190b4b54f5cbf2d2007db
otorres828 <olivertorres1997@gmail.com>
2024-04-28 10:44:21 -0600

    subida

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,6 +1,8 @@
 import product1 from '../../images/red_velvet.jpg';
 import product2 from '../../images/bread_banana.jpg';
 import product3 from '../../images/carrot_cake.jpg';
+import product4 from '../../images/caramelo_salado.jpg';
+import product5 from '../../images/coco_cake.jpg';
 import sweet1 from '../../images/frutos_rojos.jpg';
 import sweet2 from '../../images/tiramisu.jpg';
 import sweet3 from '../../images/maracuya.jpg';
@@ -10,25 +12,19 @@ export const productData = [
 		img: product1,
 		alt: 'Manejo de Redes Sociales',
 		name: 'Manejo de Redes Sociales',
-		desc: 'Aprende a manejar tus redes sociales con 4 modulos',
-		price: '10$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: product2,
 		alt: 'Pizza',
 		name: 'Contenido Estrategico',
-		desc: 'Aprende contenido estrategico con 4 modulos',
-		price: '10$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: product3,
 		alt: 'Pizza',
 		name: 'Scala Tu Brandin',
-		desc: 'Aprende a scalar tu brandin con 3 pasos',
-		price: '10$',
-		button: 'Añadir',
+		
 	},
 ];
 
@@ -37,48 +33,48 @@ export const productDataTwo = [
 		img: sweet1,
 		alt: 'Frutos rojos',
 		name: 'Frutos Rojos',
-		desc: '5-7 fotos - Entrega formato digital',
-		price: '11,25$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: sweet2,
 		alt: 'Tiramisú',
 		name: 'Tiramisú',
-		desc: '8-10 fotos - Entrega formato digital',
-		price: '18,25$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: sweet3,
 		alt: 'Maracuyá',
 		name: 'Maracuyá',
-		desc: '30-50 fotos - Entrega formato digital',
-		price: '39,99$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: product1,
 		alt: 'Red velvet',
 		name: 'Red velvet',
-		desc: 'Aprende a manejar tus redes sociales con 4 modulos',
-		price: '10$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: product2,
 		alt: 'Bread Banana Vegano',
 		name: 'Bread Banana Vegano',
-		desc: 'Aprende contenido estrategico con 4 modulos',
-		price: '10$',
-		button: 'Añadir',
+		
 	},
 	{
 		img: product3,
 		alt: 'Bread Banana Vegano',
 		name: 'Carrot Cake',
-		desc: 'Aprende a scalar tu brandin con 3 pasos',
-		price: '10$',
-		button: 'Añadir',
+
+	},
+	{
+		img: product4,
+		alt: 'Caramelo Salado',
+		name: 'Caramelo Salado',
+	
+	},
+	{
+		img: product5,
+		alt: 'Coco Cake',
+		name: 'Coco Cake',
+	
 	},
 ];
