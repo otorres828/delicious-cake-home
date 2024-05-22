3b5e7c8be4ff163edb60a679413221a469ab940f
otorres828 <olivertorres1997@gmail.com>
2024-05-22 08:49:32 -0600

    aumentar productos de categorias

--- src/components/Products/data.js
+++ src/components/Products/data.js
@@ -1,70 +1,57 @@
 import product1 from '../../images/red_velvet.jpg';
 import product2 from '../../images/bread_banana.jpg';
 import product3 from '../../images/carrot_cake.jpg';
-import product4 from '../../images/caramelo_salado.jpg';
 import product5 from '../../images/coco_cake.jpg';
-import sweet1 from '../../images/frutos_rojos.jpg';
-import sweet2 from '../../images/tiramisu.jpg';
-import sweet3 from '../../images/maracuya.jpg';
+import product6 from '../../images/oreo.jpg';
+import c1 from '../../images/frutos_rojos.jpg';
+import c2 from '../../images/tiramisu.jpg';
+import c3 from '../../images/maracuya.jpg';
+import c4 from '../../images/caramelo_salado.jpg';
 
-export const productData = [
-	{
-		img: product1,
-		alt: 'Manejo de Redes Sociales',
-		name: 'Manejo de Redes Sociales',
-		
-	},
-	{
-		img: product2,
-		alt: 'Pizza',
-		name: 'Contenido Estrategico',
-		
-	},
-	{
-		img: product3,
-		alt: 'Pizza',
-		name: 'Scala Tu Brandin',
-		
-	},
-];
+
 
 export const productCheesecake = [
 	{
-		img: sweet1,
+		img: c1,
 		alt: 'Frutos rojos',
 		name: 'Frutos Rojos',
 		
 	},
 	{
-		img: sweet3,
+		img: c2,
 		alt: 'Maracuyá',
 		name: 'Maracuyá',
 		
 	},
 	{
-		img: product1,
+		img: c3,
 		alt: 'Fresa',
 		name: 'Fresa',
 		
 	},
 	{
-		img: product4,
+		img: c4,
 		alt: 'Caramelo Salado',
 		name: 'Caramelo Salado',
 	
 	},
+	{
+		img: c4,
+		alt: 'Lotus',
+		name: 'Lotus',
+	
+	},
+	{
+		img: c4,
+		alt: 'Pistacho',
+		name: 'Pistacho',
+	
+	},
 	
 ];
-
 
 export const productTarta1kg2kg = [
 	{
-		img: product5,
-		alt: 'Coco Cake',
-		name: 'Coco Cake',
-	
-	},
-	{
 		img: product1,
 		alt: 'Red velvet',
 		name: 'Red velvet',
@@ -76,97 +63,71 @@ export const productTarta1kg2kg = [
 		name: 'Carrot Cake',
 
 	},
-
-];
-
-
-export const productTarta1kg = [
-
-	{
-		img: product1,
-		alt: 'Red velvet',
-		name: 'Red velvet',
-		
-	},
-	{
-		img: product3,
-		alt: 'Carrot Cake',
-		name: 'Carrot Cake',
-
-	},
+	
 	{
 		img: product5,
 		alt: 'Coco Cake',
 		name: 'Coco Cake',
 	
 	},
-];
+	{
+		img: product6,
+		alt: 'Oreo Cake',
+		name: 'Oreo Cake',
+	
+	},
+	{
+		img: product5,
+		alt: 'Selva Negra Cake',
+		name: 'Selva Negra Cake',
+	
+	},
+	{
+		img: product5,
+		alt: 'Milhojas Dulce de Leche',
+		name: 'Milhojas Dulce de Leche',
+	
+	},
+	{
+		img: product5,
+		alt: 'Porción de Amor (chocolate con dulce de leche)',
+		name: 'Porción de Amor (chocolate con D/L)',
+	
+	},
+	{
+		img: product5,
+		alt: 'Lemon Cake',
+		name: 'Lemon Cake',
+	
+	},
+	{
+		img: product2,
+		alt: 'Bread Banana Vegano',
+		name: 'Bread Banana Vegano',
+		
+	},
+	{
+		img: product2,
+		alt: 'Flor de Otoño (Tarta/Quesillo)',
+		name: 'Flor de Otoño (Tarta/Quesillo)',
+		
+	},
+	{
+		img: product2,
+		alt: 'Pasión Cake',
+		name: 'Pasión Cake (Tarta/Maracuyá)',
+		
+	},
 
+];
 
 export const product3Leches = [
 	{
-		img: sweet2,
+		img: c2,
 		alt: 'Tiramisú',
 		name: 'Tiramisú',
 		
 	},
-	{
-		img: product2,
-		alt: 'Bread Banana Vegano',
-		name: 'Bread Banana Vegano',
-		
-	},
-
-];
 
 
-export const productDataTwo = [
-	{
-		img: sweet1,
-		alt: 'Frutos rojos',
-		name: 'Frutos Rojos',
-		
-	},
-	{
-		img: sweet2,
-		alt: 'Tiramisú',
-		name: 'Tiramisú',
-		
-	},
-	{
-		img: sweet3,
-		alt: 'Maracuyá',
-		name: 'Maracuyá',
-		
-	},
-	{
-		img: product1,
-		alt: 'Red velvet',
-		name: 'Red velvet',
-		
-	},
-	{
-		img: product2,
-		alt: 'Bread Banana Vegano',
-		name: 'Bread Banana Vegano',
-		
-	},
-	{
-		img: product3,
-		alt: 'Carrot Cake',
-		name: 'Carrot Cake',
-
-	},
-	{
-		img: product4,
-		alt: 'Caramelo Salado',
-		name: 'Caramelo Salado',
-	
-	},
-	{
-		img: product5,
-		alt: 'Coco Cake',
-		name: 'Coco Cake',
-	
-	},
 ];
